ec5188ae84741d186c46426caed401cfd678ed25
oujeon <oujeon@gmail.com>
2023-06-21 20:41:36 +0900

    날짜 추가

--- app/view/MyViewport.js
+++ app/view/MyViewport.js
@@ -147,6 +147,19 @@ Ext.define('ExtJs6ExamplesWorld.view.MyViewport', {
                                     workspace.add(o);
                                 },
                                 text: 'Number Field'
+                            },
+                            {
+                                xtype: 'menuitem',
+                                handler: function(item, e) {
+                                    var workspace = Ext.ComponentQuery.query('container[itemId=workspace]')[0];
+
+                                    var o = Ext.create('ExtJs6ExamplesWorld.view.DateField');
+
+                                    //
+                                    workspace.removeAll();
+                                    workspace.add(o);
+                                },
+                                text: 'Date Field'
                             }
                         ]
                     }
